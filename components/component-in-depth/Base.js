f702a1a284259b412370c13c9c2c4563d1259275
hblab-tuannv <tuannv@hblab.vn>
2022-04-01 10:39:01 +0700

    Component In-Depth: Props

--- components/component-in-depth/Base.js
+++ components/component-in-depth/Base.js
@@ -1,16 +1,36 @@
-import Registration from "./Registration.js"
+import Registration from "./Registration.js";
+import Props from "./Props.js";
 
 export default {
   // Local Component Registration
   components: {
     LocalRegistration: Registration,
+
+    BlogPost: Props,
   },
   data() {
-    return {};
+    return {
+      post: {
+        title: "post.title",
+        author: {
+          name: "post.author.name",
+        },
+      },
+    };
   },
   template: `
     Global: <GlobalRegistration />
     <br/>
     Local: <LocalRegistration />
+    <br/><br/><br/>
+
+    <!-- props passed as static values -->
+    <BlogPost title="My journey with Vue" />
+
+    <!-- Dynamically assign the value of a complex expression -->
+    <BlogPost :title="post.title + ' by ' + post.author.name" />
+
+    <!-- Binding Multiple Properties Using an Object -->
+    <BlogPost v-bind="post" />
   `,
 };
